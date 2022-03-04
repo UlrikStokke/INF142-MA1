0dc5469eab17e5f088caa03052f51c9836bd7d4e
ulrik <ust005@uib.no>
2022-03-04 17:35:14 +0100

    print champions

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 from champlistloader import load_some_champs
 from core import Match
 from core import Team
+from core import Champion
 
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
@@ -9,25 +10,43 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 champions = load_some_champs()
 
 
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
+    print("Server listening.")
     s.listen()
     conn1, addr1 = s.accept()
-    print("Waiting for player 2")
-    s.listen()
-    conn2, addr2 = s.accept()
-    message1 = "hei"
-    message2 = "hoi"
-    conn1.sendall(champions.encode())
-    conn2.sendall(champions.encode())
+    #print("Waiting for player 2")
+    #s.listen()
+    #conn2, addr2 = s.accept()
+    #while True:
+    for i in champions:
+        c = champions[i]
+        tpl = c.str_tuple
+        print(tpl)
+        for n in tpl:
+            print("String sent:" + n)
+            conn1.sendall(n.encode())
+            data = conn1.recv(1024)
+    
+    conn1.sendall("Finito".encode())
+    
+    while True:
+        data = conn1.recv(1024)
+
+ 
+        #data1 =conn1.recv(1024)
+        #if not data1:
+        #    break
+        #print(data1)
+        #for i in champions
+        #conn1.sendall(data1)
 
 
-
-
-
-
-
-
+    #message1 = "hei"
+    #message2 = "hoi"
+    #conn1.sendall(s.encode())
+    #conn2.sendall(s.encode())
 
 
     #with conn1:
